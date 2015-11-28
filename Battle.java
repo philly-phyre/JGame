9206a5a62f19c3aa81e92af8fa3dd13c320684f0
Phil Carpenter <Phil Carpenter>
2015-11-28 11:59:54 -0500

    Battle.java update

--- Battle.java
+++ Battle.java
@@ -2,6 +2,7 @@ public class Battle {
 	
 	Enemy enemy;
 	User.Player TITLE = User.Player.TITLE;
+	String CLASS = User.Player.CLASS.desc;
 	
 	void pickEnemy() {
 		int x = (int)(Math.random() * 7);
@@ -12,16 +13,79 @@ public class Battle {
 		pickEnemy();
 		sect("You are under attack, " + TITLE);
 		enemy.putStats();
-		
+		try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+		sub();
 		
 	} // end battle(); //
 	
 	
 	public static String options[] = {"ATTACK", "DEFEND", "ITEMS", "FLEE"};
+	public void putOptions() {
+		String input;
+		sub(options[0] + "\t" + options[1], options[2] + "\t" + options[3]);
+		pl("");
+		pl("\t What shall you do?! \n");
+		input = TextIO.getlnWord();
+		for(String x : options){
+			while(!input.equals(x)){
+				pl("\t Please choose a valid option!");
+				input = TextIO.getlnWord();
+			} // end while //
+		} // end for:each //
+		switch(input){
+		case "ATTACK":
+			if(CLASS.equals("SCORCHER") || CLASS.equals("VOIDED")){
+				pl("\t ATTACK \n \t MAGIC");
+				input = TextIO.getlnWord();
+				if(input.equals("ATTACK")){
+					enemy.health -= attack();
+				} else {
+					// magicOptions();
+				}
+			} else {
+				enemy.health -= attack();
+			}
+			break;
+		case "DEFEND":
+			
+			break;
+		case "ITEMS":
+			
+			break;
+		case "FLEE":
+			int x = (int)(Math.random() * 5);
+			if(x < 1){
+				sub("There is no escape this round...");
+			} else {
+				sub("You have successfully escaped from the " + enemy + ", " + TITLE);
+				enemy.health = 0;
+			}
+		}
+	} // end putOptions() //
 	
-	public static Attack() {
-		
-	}
+	
+	public int attack() {
+		int LEVEL = User.LEVEL;
+		int ATTACK = 0;		
+		switch(CLASS){
+		case "BASTION":
+			ATTACK = (int)((Math.random() * LEVEL + 1) + (Math.random() * LEVEL +8));
+		case "TITAN":
+			ATTACK = (int)((Math.random() * LEVEL + 2) + (Math.random() * LEVEL + 15));
+		case "SCORCHER":
+			ATTACK = (int)((Math.random() * LEVEL) +  (Math.random() * LEVEL + 3));
+		case "PANZER":
+			ATTACK = (int)((Math.random() * LEVEL + 1) + (Math.random() * LEVEL + 10));
+		case "VOIDED":
+			ATTACK = (int)((Math.random() * LEVEL) + (Math.random() * LEVEL + 3));
+			}
+		return ATTACK;
+		}	
+
 	
 		
 	
@@ -56,4 +120,13 @@ public class Battle {
 			Thread.currentThread().interrupt();
 		}
 	} // end sleep() //
+	
+	static boolean contains(String x) {
+		for(Class c : Class.values()) {
+			if(x.equals(c.toString()))
+				return true;
+		}
+		return false;
+	} // end contains() //
+	
 }
