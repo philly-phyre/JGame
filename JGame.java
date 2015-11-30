ccd1e994e83e807f6a8c3e9560ff4d962552eb3c
Phil Carpenter <Phil Carpenter>
2015-11-29 22:18:21 -0500

    Reversion

--- JGame.java
+++ JGame.java
@@ -51,6 +51,7 @@ public class JGame {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 		
 		
 <<<<<<< HEAD
@@ -61,6 +62,9 @@ public class JGame {
 =======
 		sleep(5700);
 >>>>>>> fc451edfaaf0ace92c229745343a9cc62173f599
+=======
+		sleep(700);
+>>>>>>> parent of fc451ed... Update 18: Basic game functional!
 =======
 		sleep(700);
 >>>>>>> parent of fc451ed... Update 18: Basic game functional!
