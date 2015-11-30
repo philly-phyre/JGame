9b3b81fd5963cc6d612adea4aded88574f7c86a1
Phil Carpenter <Phil Carpenter>
2015-11-29 21:56:37 -0500

    Update 19

--- Battle.java
+++ Battle.java
@@ -55,6 +55,7 @@ public class Battle {
 						}
 						if(enemy.health <= 0){
 							pl("\t You have defeated the " + enemy + ", " + TITLE + "!");
+							sleep(3000);
 							break BATTLE;
 						}
 						break;
@@ -71,8 +72,9 @@ public class Battle {
 							sub("There is no escape this round...");
 						} else {
 							sub("You have successfully escaped from the " + enemy + ", " + TITLE);
+							sleep(3000);
 							break BATTLE;
-						}
+						} // lol/
 					} // end switch(input) //	
 					} else if(!(options.contains(input))) {
 						pl("\t Please choose a valid option!");
