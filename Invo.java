d430abaadfb5b8cc3682e3230175ad6d39f1836b
Phil Carpenter <Phil Carpenter>
2015-12-07 11:09:53 -0500

    Inevntory Class

--- Invo.java
+++ Invo.java
@@ -6,16 +6,20 @@ import java.util.List;
 public class Invo {
 	
 	
-	//leggo 
+	String TROPHIES = null; //so eclipse won't complain.
 	
 	public final String[][] pockets = {{"STATUS CHANGERS", "RUNES", "KEY ITEMS", "TROPHIES"},
 			{"POTION", "SUPER POTION", "FULL HEAL"},{"ARMOR+","ATTACK+","EXP+","HEALTH+", "DISPELL+"},
-			{"QUEST ITEMS","STORY ITEMS","JOURNAL"},{""}};
+			{"QUEST ITEMS","STORY ITEMS","JOURNAL"},{TROPHIES}}; //about this
 	
 	
 	public final List<String[]> Pockets = Arrays.asList(pockets); //why couldn't I define it in the method, I wonder?
 	
-	public static Stringg
+	public static String getPockets() {
+		return ""; //and this
+		
+		
+	}
 	
 	
 	
